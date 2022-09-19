a7c675edaa46747cfc2a28e643003f39b9858d10
fivetran-reneeli <renee.li@fivetran.com>
2022-09-19 15:26:21 -0400

    fix cte

--- models/stg_amplitude__event.sql
+++ models/stg_amplitude__event.sql
@@ -76,7 +76,7 @@ final as (
 
     select
         *,
-        {{ dbt_utils.surrogate_key(['event_id','session_id']) }} as unique_event_id,
+        {{ dbt_utils.surrogate_key(['event_id','session_id']) }} as unique_event_id
     from final_pre
 )
 
